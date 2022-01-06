cb1513ce27471f3aa83cb7694ba340e6bfce9261
Mwita Chacha <jonahhchacha@gmail.com>
2022-01-06 20:01:23 +0300

    Adding registration urls

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.contrib.auth import views 
+from django.contrib.auth.views import LoginView, logout_then_login, LogoutView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('watch.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('logout/', views.LogoutView.as_view(next_page='login'), name='logout'),    
 ]
+
--- watch/views.py
+++ watch/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
 def index(request):
     
     return render(request, 'index.html')
