dc0d888da506bdacaf2c29ade068a54bef5bca7c
Mwita Chacha <jonahhchacha@gmail.com>
2022-01-06 19:49:34 +0300

    Adding a database to the project

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -83,8 +83,10 @@ WSGI_APPLICATION = 'neighbourhood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nwatch',
+        'USER': 'postgres',
+    'PASSWORD':'6775',
     }
 }
 cloudinary.config( 
