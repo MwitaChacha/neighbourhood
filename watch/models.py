bbc861b85502b376f5e21d091bf4dcef8cedd6fa
Mwita Chacha <jonahhchacha@gmail.com>
2022-01-06 20:09:43 +0300

    Creating the NeighbourHood model

--- watch/models.py
+++ watch/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from tinymce.models import HTMLField
+from django.db.models import Q
 
 # Create your models here.
+
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    admin = models.ForeignKey(User,on_delete = models.CASCADE,related_name='administration',null=True)
+    image = CloudinaryField('image')
+    description = models.CharField(max_length=250)
+    occupants = models.IntegerField(default=0, null=True, blank=True)
+    posted_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name 
