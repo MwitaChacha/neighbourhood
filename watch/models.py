3c126a6980612b17209b0558b95a7bd5f333a3fe
Mwita Chacha <jonahhchacha@gmail.com>
2022-01-06 20:12:09 +0300

    Creating the Profile model

--- watch/models.py
+++ watch/models.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 
 # Create your models here.
 
+# NeighbourHood Model
 class NeighbourHood(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
@@ -44,3 +45,17 @@ class NeighbourHood(models.Model):
         """
         neighbourhood = cls.objects.filter(id=id).update(id=id)
         return neighbourhood       
+
+# Profile Model
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = CloudinaryField('image')
+    bio = models.CharField(max_length=250)
+    email =  models.CharField(max_length=60)
+    phone_number = models.IntegerField(blank=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='neighbour', blank=True)
+    posted_at = models.DateTimeField(auto_now=True)
+        
+    def __str__(self):
+        return self.user    
