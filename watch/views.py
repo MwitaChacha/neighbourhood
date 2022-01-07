403abccb4ef0688e78b99820a03a706bc89daa74
Mwita Chacha <jonahhchacha@gmail.com>
2022-01-07 07:27:38 +0300

    Creating index view

--- watch/views.py
+++ watch/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
+from .models import *
+from .forms import *
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 @login_required(login_url='/accounts/login/')
 def index(request):
-    
-    return render(request, 'index.html')
+    current_user = request.user
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user = request.user
+        profile =Profile.objects.get(user=current_user)
+        
+    except ObjectDoesNotExist:
+        return redirect('update_profile')
+    profiles = Profile.objects.filter(id = current_user.id).all()
+    hoods = NeighbourHood.objects.all().order_by('-posted_at')    
+    return render(request, 'index.html',{"profiles": profiles, "hoods":hoods})
