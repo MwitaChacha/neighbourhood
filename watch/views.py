6fd9c58c4af0f36239d4e92af35b862fd3499859
Mwita Chacha <jonahhchacha@gmail.com>
2022-01-07 07:31:33 +0300

    Adding update-profile view and url

--- watch/views.py
+++ watch/views.py
@@ -15,9 +15,25 @@ def index(request):
             return redirect('/accounts/login/')
         current_user = request.user
         profile =Profile.objects.get(user=current_user)
-        
     except ObjectDoesNotExist:
         return redirect('update_profile')
     profiles = Profile.objects.filter(id = current_user.id).all()
     hoods = NeighbourHood.objects.all().order_by('-posted_at')    
     return render(request, 'index.html',{"profiles": profiles, "hoods":hoods})
+
+
+
+@login_required(login_url='/accounts/login/')
+def update_profile(request):
+    current_user = request.user
+    form = ProfileForm(request.POST, request.FILES)
+    if request.method == 'POST':  
+        if form.is_valid():
+            prof = form.save(commit=False)
+            prof.user = request.user
+            prof.save()
+            return redirect ('index')
+        else:
+            form = ProfileForm()
+    return render(request, 'update-profile.html', {'form': form})
+ 
